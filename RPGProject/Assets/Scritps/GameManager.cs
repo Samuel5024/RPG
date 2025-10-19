e59d1615c072d16a26c4a85f0ff8d33e62a227ec
Samuel Fuhrman <148499762+Samuel5024@users.noreply.github.com>
2025-10-18 19:04:56 -0700

    Create _GameManager & GameManager.cs

---
+++ RPGProject/Assets/Scritps/GameManager.cs
@@ -0,0 +1,54 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using Photon.Pun;
+using Photon.Realtime;
+using System.Linq;
+
+public class GameManager : MonoBehaviourPun
+{
+
+    [Header("Players")]
+    public string playerPrefabPath;
+    public Transform[] spawnPoints;
+    public float respawnTime;
+
+    private int playersInGame;
+
+    // instance
+    public static GameManager instance;
+
+    void Awake()
+    {
+        instance = this;    
+    }
+
+    [PunRPC]
+    void ImInGame()
+    {
+        playersInGame++;
+
+        if(playersInGame == PhotonNetwork.PlayerList.Length)
+        {
+            SpawnPlayer();
+        }
+    }
+    // Start is called once before the first execution of Update after the MonoBehaviour is created
+    void Start()
+    {
+        photonView.RPC("ImInGame", RpcTarget.AllBuffered);    
+    }
+
+    void SpawnPlayer()
+    {
+        GameObject playerObj = PhotonNetwork.Instantiate(playerPrefabPath, spawnPoints[Random.Range(0, spawnPoints.Length)].position, Quaternion.identity);
+
+        // initialize the player
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
